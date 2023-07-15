66914653a32fc978f0964cd27f19506fc8b74485
Mohanisha P <48022750+mohanisha22397@users.noreply.github.com>
2023-07-15 13:42:25 +0530

    Create HDFC.java

---
+++ Bank Design Patterns/HDFC.java
@@ -0,0 +1,26 @@
+package DesignPatterns;
+
+public class HDFC extends Bank{
+private String BankName;
+private int ROI;
+
+
+
+public HDFC(String bankname, int roi) {
+	// TODO Auto-generated constructor stub
+	this.BankName=bankname;
+	this.ROI=roi;
+}
+
+public String getBankName() {
+	// TODO Auto-generated method stub
+	return this.BankName;
+}
+
+public int getROI() {
+	// TODO Auto-generated method stub
+	return this.ROI;
+}
+
+
+}
