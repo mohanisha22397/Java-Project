fde91fe57c233b7ce6488c89d96d92f8fb4618e8
Mohanisha P <48022750+mohanisha22397@users.noreply.github.com>
2023-07-15 13:43:29 +0530

    Create Bank.java

---
+++ Bank Design Patterns/Bank.java
@@ -0,0 +1,20 @@
+package DesignPatterns;
+
+public abstract class Bank {
+
+	public String getBankName() {
+		return getBankName();
+	}
+
+	public int getROI() {
+		return getROI();
+	}
+
+public String toString() {
+	return "BankName:" + this.getBankName() +" " + "ROI:" + this.getROI() ;
+}
+
+
+
+
+}
